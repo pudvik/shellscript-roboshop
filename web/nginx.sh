468f3468324c494ddad7a0341564a77857c5e4ea
pudvik <prudviksanikala54@gmail.com>
2024-07-23 13:47:14 +0530

    roboshop

--- web/nginx.sh
+++ web/nginx.sh
@@ -23,31 +23,32 @@ else
     echo "You are super user."
 fi
 
-dnf install nginx -y &>>LOGFILE
+dnf install nginx -y &>>$LOGFILE
 VALIDATE $? "Install Nginx"
 
-systemctl enable nginx &>>LOGFILE
+systemctl enable nginx &>>$LOGFILE
 VALIDATE $? "Enable Nginx"
 
-systemctl start nginx &>>LOGFILE
+systemctl start nginx &>>$LOGFILE
 VALIDATE $? "Start Nginx"
 
-rm -rf /usr/share/nginx/html/* &>>LOGFILE
+rm -rf /usr/share/nginx/html/* &>>$LOGFILE
 VALIDATE $? "Remove default html directory"
 
 curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>>LOGFILE
 VALIDATE $? " Download nginx code"
 
-cd /usr/share/nginx/html &>>LOGFILE
+cd /usr/share/nginx/html &>>$LOGFILE
 VALIDATE $? "Move to html directory"
 
-unzip /tmp/web.zip &>>LOGFILE
+unzip /tmp/web.zip &>>$LOGFILE
 VALIDATE $? "Extracting web code"
 
 cp /home/ec2-user/shellscript-roboshop/roboshop.conf /etc/nginx/default.d/roboshop.conf &>>LOGFILE
 VALIDATE $? "Copied conf file"
 
-systemctl restart nginx &>>LOGFILE
+
+systemctl restart nginx &>>$LOGFILE
 VALIDATE $? "Restart Nginx"
 
 
