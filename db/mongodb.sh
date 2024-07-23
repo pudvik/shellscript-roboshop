fe11ea8eb85fe25ad90bf3a00f9bb57f49dd753d
pudvik <prudviksanikala54@gmail.com>
2024-07-23 11:53:59 +0530

    roboshop

--- db/mongodb.sh
+++ db/mongodb.sh
@@ -16,11 +16,11 @@ VALIDATE(){
 }
 
 if [ $USERID -ne 0 ]
-then 
-    echo "Please run the script with root access"
-    exit 1
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
-    echo "You are super user"
+    echo "You are super user."
 fi
 
 cp /root/shellscript-roboshop/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
